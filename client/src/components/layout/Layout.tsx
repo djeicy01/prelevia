6b0acd6550366ad38a00112b85caffc67e19916d
djeicy01 <djeicy01@gmail.com>
2026-03-30 13:22:41 +0000

    fix(layout): use inline marginLeft to fix main content hidden behind sidebar

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
+const SIDEBAR_W = 255
+
 export default function Layout() {
   return (
     <div className="flex min-h-screen" style={{ background: '#F5F7F6' }}>
       <Sidebar />
-      <main className="flex-1 ml-[255px] flex flex-col min-h-screen">
+      <main
+        className="flex flex-col min-h-screen w-full"
+        style={{ marginLeft: SIDEBAR_W }}
+      >
         <Outlet />
       </main>
     </div>
