d676e66f361c8d14dbc8401d51415f4e8877b11d
djeicy01 <djeicy01@gmail.com>
2026-03-31 17:38:15 +0000

    Add MissionDetail page and /missions/:id route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,10 @@ import Paiements  from './pages/Paiements'
 import Agents     from './pages/Agents'
 import Missions   from './pages/Missions'
 import Stock      from './pages/Stock'
-import Rapports   from './pages/Rapports'
+import Rapports     from './pages/Rapports'
+import Parametres   from './pages/Parametres'
+import PatientDetail from './pages/PatientDetail'
+import MissionDetail from './pages/MissionDetail'
 
 export default function App() {
   return (
@@ -20,13 +23,16 @@ export default function App() {
         <Route element={<ProtectedRoute />}>
           <Route element={<Layout />}>
             <Route index             element={<Dashboard />} />
-            <Route path="patients"   element={<Patients />} />
+            <Route path="patients"        element={<Patients />} />
+            <Route path="patients/:id"    element={<PatientDetail />} />
             <Route path="assurances" element={<Assurances />} />
             <Route path="paiements"  element={<Paiements />} />
             <Route path="agents"     element={<Agents />} />
             <Route path="missions"   element={<Missions />} />
+            <Route path="missions/:id" element={<MissionDetail />} />
             <Route path="stock"      element={<Stock />} />
             <Route path="rapports"   element={<Rapports />} />
+            <Route path="parametres" element={<Parametres />} />
           </Route>
         </Route>
       </Routes>
